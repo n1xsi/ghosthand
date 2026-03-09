d6aac6df8700ee583997c1f34c0ff9ae482a82be
n1xsi <vanyalebedev45@gmail.com>
2026-03-09 18:02:42 +0300

    fix: some minor architecture fixes

--- scripts/mrc.py
+++ scripts/mrc.py
@@ -12,27 +12,27 @@ class MiniRecoilCore:
 
         self.left_button_pressed = False  # Флаг для проверки удержания левой кнопки
 
-    def move_mouse(self, dx, dy):
+    def _move_mouse(self, dx, dy):
         ctypes.windll.user32.mouse_event(0x0001, dx, dy, 0, 0)
+    
+    def _is_left_button_pressed(self):
+        return ctypes.windll.user32.GetAsyncKeyState(0x01) != 0
 
-    def mouse_listener(self):
-        while True:
-            if self.enabled and self.is_left_button_pressed():
+    def _loop(self):
+        while self.running:
+            if self.enabled and self._is_left_button_pressed():
                 self.left_button_pressed = True
             else:
                 self.left_button_pressed = False
             
             if self.left_button_pressed:
-                self.move_mouse(0, self.strength)
+                self._move_mouse(0, self.strength)
 
             time.sleep(self.speed)
 
-    def is_left_button_pressed(self):
-        return ctypes.windll.user32.GetAsyncKeyState(0x01) != 0
-
     def start(self):
         self.running = True
-        threading.Thread(target=self.mouse_listener, daemon=True).start()
+        threading.Thread(target=self._loop, daemon=True).start()
 
 # Экземпляр класса MiniRecoilCore для импорта в меню
 mrc_instance = MiniRecoilCore()
