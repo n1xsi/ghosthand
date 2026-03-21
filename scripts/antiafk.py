eea6608bdb728d53c7b5c26a5b201cbf7d176f99
n1xsi <vanyalebedev45@gmail.com>
2026-03-21 23:13:39 +0300

    fix: add smart waiting + adapted to panic key + linkage to input_sim

--- scripts/antiafk.py
+++ scripts/antiafk.py
@@ -1,5 +1,5 @@
+from src.core.input_sim import PressKey, ReleaseKey
 import threading
-import keyboard
 import time
 import random
 
@@ -9,26 +9,33 @@ class AntiAfkCore:
         self.running = False  # Глобальный переключатель (вкл/выкл поток)
         self.enabled = False  # Переключатель функционала (для чекбокса в меню)
 
-        self.action_keys = [0x11, 0x1E, 0x1F, 0x20]  # Клавиши нажатия для имитации действий (WASD)
+        # Клавиши нажатия для имитации действий (W, A, S, D)
+        self.action_keys = [0x11, 0x1E, 0x1F, 0x20]
+
+    def start(self):
+        self.running = True
+        threading.Thread(target=self._loop, daemon=True).start()
 
     def _loop(self):
         while self.running:
             if not self.enabled:
-                time.sleep(1)  # Если выключено - то долгое ожидание, чтобы не грузить CPU
+                # Если выключено - то долгое ожидание, чтобы не грузить CPU
+                time.sleep(1)
                 continue
 
             key_to_press = random.choice(self.action_keys)
             hold_time = random.uniform(0.1, 0.4)
             wait_time = random.uniform(1.0, 10.0)
 
-            keyboard.press(key_to_press)
+            PressKey(key_to_press)
             time.sleep(hold_time)
-            keyboard.release(key_to_press)
-            time.sleep(wait_time)
+            ReleaseKey(key_to_press)
 
-    def start(self):
-        self.running = True
-        threading.Thread(target=self._loop, daemon=True).start()
+            # "Умное" ожидание, чтобы реагировать на отключение галочки мгновенно
+            elapsed = 0
+            while elapsed < wait_time and self.enabled:
+                time.sleep(0.1)
+                elapsed += 0.1
 
 
 # Экземпляр класса AntiAfkCore для импорта в меню
