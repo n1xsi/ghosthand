480043f1431bb6825bc6bbd3ac0023cfd9c1ce3f
n1xsi <vanyalebedev45@gmail.com>
2026-03-21 22:17:31 +0300

    fix: add debug prints + text formatting

--- scripts/panic.py
+++ scripts/panic.py
@@ -4,14 +4,16 @@ import threading
 import time
 
 
-VK_PAUSE = 0x13 # Кнопка Pause/Break
+VK_PAUSE = 0x13  # Кнопка Pause/Break
+
 
 def panic_loop():
     was_pressed = False
 
     while True:
         # Чтение кнопки НАПРЯМУЮ через Windows, иначе пауза заблокирует чтение кнопки Паузы ))
-        current_state = (input_sim.user32.GetAsyncKeyState(VK_PAUSE) & 0x8000) != 0
+        current_state = (input_sim.user32.GetAsyncKeyState(
+            VK_PAUSE) & 0x8000) != 0
 
         if current_state and not was_pressed:
             # Переключение глобальной переменной в ядре
@@ -23,15 +25,19 @@ def panic_loop():
                 if input_sim.GLOBAL_PAUSE:
                     dpg.set_value("status_text", "GLOBAL PAUSE")
                     dpg.configure_item("status_text", color=(255, 50, 50, 255))
+                    print("[debug:panic] GLOBAL PAUSE")
                 else:
                     dpg.set_value("status_text", "SYSTEM ACTIVE")
                     dpg.configure_item("status_text", color=(50, 255, 50, 255))
-            except: pass
+                    print("[debug:panic] SYSTEM ACTIVE")
+            except:
+                pass
 
         elif not current_state:
             was_pressed = False
 
         time.sleep(0.01)
+
 
 # Автоматический запуск потока при импорте этого файла
 threading.Thread(target=panic_loop, daemon=True).start()
