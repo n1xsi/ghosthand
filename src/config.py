8d2f51884e883fccc93a94b7faa18adbd90b6ab4
nixsi <vanyalebedev45@gmail.com>
2026-05-18 22:00:00 +0300

    init: add new config module with all constants

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# Цвета темы (RGBA)
+DEEP_PURPLE      = (139, 0, 255, 255)
+SOFT_PURPLE      = (163, 102, 255, 255)
+ACTIVE_PURPLE    = (184, 102, 255, 255)
+ADDITIONAL_BLACK = (150, 150, 150, 200)
+
+# Размеры основного окна (базовые, при UI_SCALE = 1.4)
+BASE_VIEWPORT_WIDTH  = 600
+BASE_VIEWPORT_HEIGHT = 420
+
+# Позиция и отступы текста статуса
+BASE_STATUS_TEXT_POS = (440, 8)
+STATUS_RIGHT_MARGIN  = 160
+
+# UI
+UI_SCALE = 1.4
+VERSION  = "v0.9.1 Dev Build"
